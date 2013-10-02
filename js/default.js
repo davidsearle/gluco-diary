a5bc24188c318b73fed7f8e29ea47e91022cf250
David Searle <david@cosma.co.uk>
2013-10-02 21:57:45 +0100

    adding glucose readings, and switched the chart library

--- js/default.js
+++ js/default.js
@@ -1,180 +1,215 @@
-// GlucoDiary
-
-function localstore() {
-    if (typeof (Storage) !== "undefined") {
-        if (localStorage.lastname === null) {
-            localStorage.lastname = "default";
-        }
-        document.getElementById("result").innerHTML = "Last name: " + localStorage.lastname;
-        if (localStorage.clickcount) {
-            document.getElementById("result").innerHTML += "<br /><br /><br />You have clicked the button " + localStorage.clickcount + " time(s).";
-        }
-    } else {
-        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
-    }
-}
-
-function save() {
-    if (typeof (Storage) !== "undefined") {
-        if (document.getElementById("txtname").value === null) {
-            localStorage.lastname = "defaultTxtBx";
-        } else {
-            localStorage.lastname = document.getElementById("txtname").value;
-            document.getElementById("result").innerHTML = "Last name: " + localStorage.lastname;
-        }
-    } else {
-        document.getElementById("result").innerHTML = "Cannot save :(";
-    }
-
-
-    if (localStorage.clickcount) {
-        localStorage.clickcount = Number(localStorage.clickcount) + 1;
-    } else {
-        localStorage.clickcount = 1;
-    }
-
-    document.getElementById("result").innerHTML += "<br /><br /><br />You have clicked the button " + localStorage.clickcount + " time(s).";
-}
-
-
-function chart() {
-
-    //Get the context of the canvas element we want to select
-    var ctx = document.getElementById("myChart").getContext("2d");
-
-    // data
-    var data = {
-        //labels: ["21 Sep 23:45", "22 Sep 09:45", "22 Sep 11:13", "22 Sep 14:56", "22 Sep 17:54", "22 Sep 23:19", "23 Sep 11:01", "21 Sep 23:45", "22 Sep 09:45", "22 Sep 11:13", "22 Sep 14:56", "22 Sep 17:54", "22 Sep 23:19", "23 Sep 11:01", "21 Sep 23:45", "22 Sep 09:45", "22 Sep 11:13", "22 Sep 14:56", "22 Sep 17:54", "22 Sep 23:19", "23 Sep 11:01", "21 Sep 23:45", "22 Sep 09:45", "22 Sep 11:13", "22 Sep 14:56", "22 Sep 17:54", "22 Sep 23:19", "23 Sep 11:01"],
-        //labels: ["", "", "", "", "", "L", "a", "s", "t", "", "", "", "4", "0", "", "", "", "E", "n", "t", "r", "i", "e", "s", "", "", "", ""],
-        labels: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
-        datasets: [
-            {
-                fillColor: "rgba(220,220,220,0.5)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                data: [10.4, 17.2, 8.3, 13.4, 15.3, 10.5, 11.2, 10.4, 17.2, 8.3, 13.4, 15.3, 10.5, 11.2, 10.4, 17.2, 8.3, 13.4, 15.3, 10.5, 11.2, 10.4, 17.2, 8.3, 13.4, 15.3, 10.5, 11.2]
-            }
-            //                ,
-
-            //            {
-
-            //                fillColor: "rgba(151,187,205,0.5)",
-
-            //                strokeColor: "rgba(151,187,205,1)",
-
-            //                pointColor: "rgba(151,187,205,1)",
-
-            //                pointStrokeColor: "#fff",
-
-            //                data: [28, 48, 40, 19, 96, 27, 100]
-
-            //            }
-
-        ]
-    };
-
-    //options
-    var options = {
-        animationSteps: 10,
-        scaleFontSize: 9
-    };
-
-    //Line.defaults = {
-
-    //    //Boolean - If we show the scale above the chart data
-    //    scaleOverlay: false,
-
-    //    //Boolean - If we want to override with a hard coded scale
-    //    scaleOverride: false,
-
-    //    //** Required if scaleOverride is true **
-    //    //Number - The number of steps in a hard coded scale
-    //    scaleSteps: null,
-    //    //Number - The value jump in the hard coded scale
-    //    scaleStepWidth: null,
-    //    //Number - The scale starting value
-    //    scaleStartValue: null,
-
-    //    //String - Colour of the scale line
-    //    scaleLineColor: "rgba(0,0,0,.1)",
-
-    //    //Number - Pixel width of the scale line
-    //    scaleLineWidth: 1,
-
-    //    //Boolean - Whether to show labels on the scale
-    //    scaleShowLabels: true,
-
-    //    //Interpolated JS string - can access value
-    //    scaleLabel: "<%=value%>",
-
-    //    //String - Scale label font declaration for the scale label
-    //    scaleFontFamily: "'Arial'",
-
-    //    //Number - Scale label font size in pixels
-    //    scaleFontSize: 12,
-
-    //    //String - Scale label font weight style
-    //    scaleFontStyle: "normal",
-
-    //    //String - Scale label font colour
-    //    scaleFontColor: "#666",
-
-    //    ///Boolean - Whether grid lines are shown across the chart
-    //    scaleShowGridLines: true,
-
-    //    //String - Colour of the grid lines
-    //    scaleGridLineColor: "rgba(0,0,0,.05)",
-
-    //    //Number - Width of the grid lines
-    //    scaleGridLineWidth: 1,
-
-    //    //Boolean - Whether the line is curved between points
-    //    bezierCurve: true,
-
-    //    //Boolean - Whether to show a dot for each point
-    //    pointDot: true,
-
-    //    //Number - Radius of each point dot in pixels
-    //    pointDotRadius: 3,
-
-    //    //Number - Pixel width of point dot stroke
-    //    pointDotStrokeWidth: 1,
-
-    //    //Boolean - Whether to show a stroke for datasets
-    //    datasetStroke: true,
-
-    //    //Number - Pixel width of dataset stroke
-    //    datasetStrokeWidth: 2,
-
-    //    //Boolean - Whether to fill the dataset with a colour
-    //    datasetFill: true,
-
-    //    //Boolean - Whether to animate the chart
-    //    animation: true,
-
-    //    //Number - Number of animation steps
-    //    animationSteps: 60,
-
-    //    //String - Animation easing effect
-    //    animationEasing: "easeOutQuart",
-
-    //    //Function - Fires when the animation is complete
-    //    onAnimationComplete: null
-
-    //}
-
-
-    // init chart
-    var myNewChart = new Chart(ctx).Line(data, options);
-    //new Chart(ctx).Line(data, options);
-
-}
-
-
-
-//if (typeof window.innerWidth != 'undefined') {
-//                viewportwidth = window.innerWidth;
-//                viewportheight = window.innerHeight;
-//    alert(viewportwidth)
+/*jslint browser: true, devel: true, sloppy: true  */
+// GlucoDiary Namespace
+var GlucoDiary = GlucoDiary || {};
+var Chartjsorg;
+//GlucoDiary.Chart = function () {
+//    //configure chart.js
+//    document.getElementById("chartmessage").innerHTML = "Initialising chart..";
+//    var ctx, data, options, myNewChart;
+//    ctx = document.getElementById("myChart").getContext("2d");
+//    data = {
+//        labels: ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+//        datasets: [
+//            {
+//                //fillColor: "rgba(220,220,220,0.5)",
+//                fillColor: "rgba(226,111,122,0.5)",
+//                //strokeColor: "rgba(220,220,220,1)",
+//                strokeColor: "rgba(196,39,55,0.5)",
+//                pointColor: "rgba(196,39,55,0.5)",
+//                pointStrokeColor: "#fff",
+//                data: [10.4, 17.2, 8.3, 13.4, 15.3, 10.5, 11.2, 10.4, 17.2, 8.3, 13.4, 15.3, 10.5, 11.2, 10.4, 17.2, 8.3, 13.4, 15.3, 10.5, 11.2, 10.4, 17.2, 8.3, 13.4, 15.3, 10.5, 11.2]
 //            }
+//        ]
+//    };
+//    document.getElementById("chartmessage").innerHTML += "<br />data loaded...";
+//    //options
+//    options = {
+//        animationSteps: 10,
+//        scaleFontSize: 9
+//    };
+//    // init new chart
+//    myNewChart = new Chartjsorg(ctx).Line(data, options);
+//    document.getElementById("chartmessage").innerHTML += "<br />chart created (FINISHED)";
+//};
+
+
+GlucoDiary.HighChart = function () {
+    var chart = new Highcharts.Chart({
+        chart: {
+            //            animation: {
+            //                duration: 1000
+            //            },
+            renderTo: 'highcharts',
+            type: 'areaspline'
+        },
+
+        //        xAxis: {
+        //            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        //        },
+        //
+        //        series: [
+        //            {
+        //                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        //            }]
+
+        title: {
+            text: 'Last 80 entries'
+        },
+        //        subtitle: {
+        //            text: 'Irregular time data in Highcharts JS'
+        //        },
+        //        legend: {
+        //                layout: 'vertical',
+        //                align: 'left',
+        //                verticalAlign: 'top',
+        //                x: 150,
+        //                y: 100,
+        //                floating: true,
+        //                borderWidth: 1,
+        //                backgroundColor: '#FFFFFF'
+        //            },
+        xAxis: {
+            //                type: 'datetime',
+            //                categories: [
+            //                    '',
+            //                    '',
+            //                    '',
+            //                    '',
+            //                    '',
+            //                    '',
+            //                    ''
+            //                ],
+            //                plotBands: [{ // visualize the weekend
+            //                    from: 4.5,
+            //                    to: 6.5,
+            //                    color: 'rgba(68, 170, 213, .2)'
+            //                }]
+        },
+        yAxis: {
+            title: {
+                text: ''
+            }
+        },
+        //        tooltip: {
+        //            shared: true,
+        //            valueSuffix: ' units'
+        //        },
+        tooltip: {
+            enabled: false
+        },
+        credits: {
+            enabled: false
+        },
+        plotOptions: {
+            areaspline: {
+                fillOpacity: 0.5
+            }
+        },
+        series: [{
+            states: {
+                hover: {
+                    enabled: false
+                }
+            },
+            animation: false,
+            name: 'Glucose',
+            data: [11.1, 14.3, 13.3, 25, 14, 10, 12, 9.1, 5.1, 2.1]
+        }
+                 //                     , {
+                 //                name: 'Carbs',
+                 //                data: [7, 5, 4, 6, 6, 5, 4, 10, 3, 7]
+                 //            }, {
+                 //                name: 'Rapid Insulin',
+                 //                data: [6, 5, 4, 7, 6, 5, 4, 8, 2, 6]
+                 //            }
+
+            ]
+    });
+};
+
+
+
+///// INIT /////
+window.onload = function () {
+    GlucoDiary.CheckSupportsStorage();
+    GlucoDiary.CheckSupportsAppCache();
+    GlucoDiary.CheckIfOnline();
+    GlucoDiary.InitInteractionListeners();
+    //GlucoDiary.Chart();
+    GlucoDiary.HighChart();
+};
+
+
+
+/////////   EVENT LISTENERS    ////////
+GlucoDiary.InitInteractionListeners = function () {
+    document.getElementById("GDcarbs").onclick = function () {
+        alert('record carbs');
+    };
+    document.getElementById("GDinsulin").onclick = function () {
+        alert('record insulin');
+    };
+    document.getElementById("GDglucose").onclick = function () {
+        document.getElementById("inputGlucoseReading").scrollIntoView();
+    };
+};
+
+/////////   CAPABILITIES     //////////
+
+GlucoDiary.CheckSupportsStorage = function () {
+    //Check if localStorage is supported:
+    if (window.localStorage) {
+        document.getElementById("infomessage").innerHTML += "Local Storage is available for use<br />";
+    }
+};
+
+GlucoDiary.CheckIfOnline = function () {
+    //Check if app is online
+    if (navigator.onLine) {
+        document.getElementById("errormessage").innerHTML += "Online<br />";
+    } else {
+        document.getElementById("errormessage").innerHTML += "Offline<br />";
+    }
+};
+
+GlucoDiary.CheckSupportsAppCache = function () {
+    //Check if offline application cache is supported:
+    if (window.applicationCache) {
+        document.getElementById("infomessage").innerHTML += "Application Cache is available for use<br />";
+    }
+};
+
+
+////////  FUNCTIONS   /////////////
+
+GlucoDiary.SaveGlucoseReading = function () {
+    if (typeof (Storage) !== "undefined") {
+        //get datetime
+        var d, dFormatted;
+        d = new Date();
+        dFormatted = [d.getFullYear(), ("0" + d.getMonth()).slice(-2), ("0" + d.getDate()).slice(-2), "_", ("0" + d.getUTCHours()).slice(-2), ("0" + d.getUTCMinutes()).slice(-2), ("0" + d.getUTCSeconds()).slice(-2)].join("");
+        // check that values aren't all zeroes
+        if (document.getElementById("glucoseTens").value === "0" && document.getElementById("glucoseUnits").value === "0" && document.getElementById("glucoseDecimal").value === "0") {
+            document.getElementById("errormessage").innerHTML = "Please select a value before clicking SAVE";
+            document.getElementById("errormessage").scrollIntoView();
+
+        } else {
+            //
+            if (document.getElementById("glucoseTens").value === "0") {
+                //localStorage.lastname = document.getElementById("glucoseUnits").value + "." + document.getElementById("glucoseDecimal").value;
+                localStorage["gluco_" + dFormatted] = document.getElementById("glucoseUnits").value + "." + document.getElementById("glucoseDecimal").value;
+            } else {
+                //localStorage.lastname = document.getElementById("glucoseTens").value + document.getElementById("glucoseUnits").value + "." + document.getElementById("glucoseDecimal").value;
+                localStorage["gluco_" + dFormatted] = document.getElementById("glucoseTens").value + document.getElementById("glucoseUnits").value + "." + document.getElementById("glucoseDecimal").value;
+            }
+            document.getElementById("glucoseTens").value = "0";
+            document.getElementById("glucoseUnits").value = "0";
+            document.getElementById("glucoseDecimal").value = "0";
+            document.getElementById("errormessage").innerHTML = "";
+            document.getElementById("container").scrollIntoView();
+        }
+    } else {
+        document.getElementById("errormessage").innerHTML += "Cannot save :(<br />";
+        document.getElementById("errormessage").scrollIntoView();
+    }
+};
--- manifest.php
+++ manifest.php
@@ -1,9 +1,13 @@
 <?php header("Content-type: text/cache-manifest"); ?>
 CACHE MANIFEST
-#v2.001
+#v2.100031234567
 
 CACHE:
 default.htm
 css/default.css
+css/buttons.css
+images/icon-sprite.png
+images/icon-sprite@2x.png
 js/default.js
-js/chart.min.js
+js/highcharts.js
+js/standalone-framework.js
